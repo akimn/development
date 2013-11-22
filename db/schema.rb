fb1936694ef57f4aff71e0ea18c469829cdd30fc
Andrew Kimn <akimn89@gmail.com>
2013-11-21 23:36:55 -0800

    fixed it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131122050635) do
+ActiveRecord::Schema.define(version: 20131122071004) do
 
   create_table "pins", force: true do |t|
     t.string   "locationBuilding"
