18ae083056de4a7c0f8eb25ea41d451a9f8f3fae
Andrew Kimn <akimn89@gmail.com>
2013-11-21 21:54:51 -0800

    amazon s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Development::Application.configure do
 
   #set to actual host name
   config.action_mailer.default_url_options = { :host => 'http://development123.herokuapp.com/' }
+
+  #sets paperclip to upload pics to amazaon s3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
