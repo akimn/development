a499f0b49f502120c8a00ecedd63ca17e9b5d40c
Andrew Kimn <akimn89@gmail.com>
2013-11-21 21:38:39 -0800

    pics

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131122034511) do
+ActiveRecord::Schema.define(version: 20131122050635) do
 
   create_table "pins", force: true do |t|
     t.string   "locationBuilding"
     t.string   "concern"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
