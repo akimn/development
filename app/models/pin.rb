4765a603ac4d7686abc114714676635c64bda5d7
Andrew Kimn <akimn89@gmail.com>
2013-11-24 22:32:29 -0800

    finished I hope

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,8 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "7000x7000>" }
+
+	 validates :image, presence: true
+	 validates :locationBuilding, presence: true
+	 validates :concern, presence: true
 end
